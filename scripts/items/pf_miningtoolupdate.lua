cb817183b5da0fdb4e5219b58791a255e81f93ea
Pixelflame5826 <epicuniverse999@gmail.com>
2025-05-22 20:07:05 -0500

    Biome Changes + Bug Fixes + Beginning of Weapon Changes

--- scripts/items/pf_miningtoolupdate.lua
+++ scripts/items/pf_miningtoolupdate.lua
@@ -2,7 +2,7 @@ function ageItem(baseItem, aging)
 
   baseItem.parameters.tooltipFields = baseItem.parameters.tooltipFields or {}
 
-  baseItem.parameters.currentDurability = baseItem.parameters.maxDurability - (baseItem.parameters.durabilityHit or 0)
+  baseItem.parameters.currentDurability = (baseItem.parameters.maxDurability or 0) - (baseItem.parameters.durabilityHit or 0)
 
   baseItem.parameters.tooltipFields.durabilityLabel = string.format("%s / ^gray;%s", baseItem.parameters.currentDurability, baseItem.parameters.maxDurability)
 
